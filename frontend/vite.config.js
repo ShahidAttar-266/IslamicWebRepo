4abd94e4a6d18ef960841bb7adae9aa258359770
shahid attar <shahidattar266@gmail.com>
2026-05-09 15:37:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // raise warning threshold to 1000kb
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react-dom') || id.includes('react-router')) return 'react-vendor'
-            if (id.includes('react')) return 'react-vendor'
-            if (id.includes('@tanstack')) return 'query-vendor'
-            if (id.includes('lucide') || id.includes('react-hot-toast')) return 'ui-vendor'
-            if (id.includes('react-hook-form') || id.includes('zod') || id.includes('@hookform')) return 'form-vendor'
-            return 'vendor'
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 2000,
   }
 })
